0330aeb7d36e5ce9d8a010e4bb7336ded5a23ef6
almokoushik <mottaki567@gmail.com>
2021-02-25 11:11:14 +0600

    adding a new react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload If you are not using Live server.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React From Expert
         </a>
       </header>
     </div>
